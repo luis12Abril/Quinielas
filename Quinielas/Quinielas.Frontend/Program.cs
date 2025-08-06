640cdcf427e44947747bf47e83d89f72b1478a30
Luis12Abril <luisarturobarreras@gmail.com>
2025-08-06 12:40:31 -0700

    Hasta el video 6

--- Quinielas/Quinielas.Frontend/Program.cs
+++ Quinielas/Quinielas.Frontend/Program.cs
@@ -1,3 +1,4 @@
+using Quinielas.Frontend.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Quinielas.Frontend;
@@ -6,6 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7228") });
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
